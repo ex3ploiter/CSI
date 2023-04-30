991fc4ab58e2d4e30c09e74ebcb37744054574c9
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-04-30 15:38:02 +0330

    lars

--- common/common.py
+++ common/common.py
@@ -40,7 +40,7 @@ def parse_args(default=False):
                         default=1000, type=int)
     parser.add_argument('--optimizer', help='Optimizer',
                         choices=['sgd', 'lars'],
-                        default='lars', type=str)
+                        default='sgd', type=str)
     parser.add_argument('--lr_scheduler', help='Learning rate scheduler',
                         choices=['step_decay', 'cosine'],
                         default='cosine', type=str)
